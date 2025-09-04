54b24e724c7a6fe749c3de0e1102150e4d24379f
Juan Sebastian Pabon Mojica <nova159712@gmail.com>
2025-09-04 17:54:09 -0500

    Añade las paginas y su routing, asi como los css modules asociados a cada componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+
+//Routear de forma declarativa
 function App() {
-  return <div>hola</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='product'
+          element={<Product />}
+        />
+
+        <Route
+          path='pricing'
+          element={<Pricing />}
+        />
+        <Route
+          path='/'
+          element={<HomePage />}
+        />
+        <Route
+          path='/login'
+          element={<Login />}
+        />
+        <Route
+          path='*'
+          element={<PageNotFound />}
+        />
+
+        <Route
+          path='app'
+          element={<AppLayout />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
